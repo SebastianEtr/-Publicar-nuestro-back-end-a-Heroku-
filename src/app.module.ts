8808d2757cb445e5e02c16ae732870ae339ef3d3
sebaseter <sebaslopezz654@gmail.com>
2022-09-05 18:16:37 -0500

    end-point-health

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 import { PetController } from './pet/pet.controller';
 import { PetModule } from './pet/pet.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 @Module({
-imports: [PetModule],
+imports: [PetModule,
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client'),
+  }),],
   controllers: [AppController, HealthController, PetController],
   providers: [AppService],
 })
